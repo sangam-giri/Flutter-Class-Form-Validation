3be615d6531fbf09e8f80d0bc149d55728b69ffb
Sangam Giri <girisangam00@gmail.com>
2023-01-27 18:45:08 +0545

    intial commit

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Something';
+                      }
+                    }),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Something';
+                      }
+                      if (!value!.contains('@')) {
+                        return 'Please enter valid email';
+                      }
+                    }),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ],
+              )),
+          ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  print('Validated');
+                } else {
+                  print('Not Validated');
+                }
+              },
+              child: const Text('Submit')),
+        ],
+      ),
+    );
+  }
+}
